ec097d6dcd774bde2196a84fd63ce68fdab6e194
drew-dirac <drew@diracinc.com>
2025-02-07 14:47:55 -0500

    correct configuration not module build

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Custom NixOS WSL build using nixos-24.11";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # Override the nixos-wsl flake’s nixpkgs input to point to nixos-24.11.
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
@@ -12,15 +13,15 @@
     };
   };
 
-  outputs = { self, nixos-wsl, ... }:
+  outputs = { self, nixos-wsl, nixpkgs, ... }:
     let
       system = "x86_64-linux";
     in {
       # Build a NixOS configuration using the overridden nixos-wsl
-      nixosConfigurations.diracnix = nixos-wsl.lib.nixosSystem {
+      nixosConfigurations.diracnix = nixpkgs.lib.nixosSystem {
         system = system;
         modules = [
-          nixos-wsl.nixosModules.default
+          nixos-wsl.nixosConfigurations.default
           # You can add further customizations here (e.g. install extra packages)
         ];
       };
