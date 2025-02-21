afd50ed583b4ea7a0f4d600aa6cdc82b73850106
drew-dirac <drew@diracinc.com>
2025-02-21 20:47:02 +0000

    fix qalculate, add 1password

--- baremetal.nix
+++ baremetal.nix
@@ -15,7 +15,14 @@
     # useful gui tools
     code-cursor
     vlc
-    qalculate
+    qalculate-gtk
   ];
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    # polkitPolicyOwners = [ "yourUsernameHere" ];
+  };
 }
